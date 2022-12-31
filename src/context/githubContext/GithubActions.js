40420fa7f61d974e845cbe2962cec0b620aeee35
Franck Owen <fxfrancky@yahoo.com>
2023-01-01 00:13:41 +0100

    commit after token revoke

--- src/context/githubContext/GithubActions.js
+++ src/context/githubContext/GithubActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const GITHUB_URL = "https://api.github.com";
-const GITHUB_TOKEN = "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";
+
 const githubAxios = axios.create({
   baseURL: GITHUB_URL,
   // headers: { Authorization: `token ${GITHUB_TOKEN}` },
@@ -61,7 +61,7 @@ export const getUserRepos = async (login) => {
   const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
     headers: {
       "Content-Type": "application-json",
-      Authorization: `token ${GITHUB_TOKEN}`,
+      // Authorization: `token ${GITHUB_TOKEN}`,
     },
   });
   const data = await response.json();
